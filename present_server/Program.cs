f83bc410072e8d935d7f0c9fb837671bb178009d
Robertas Cvetinskis <robertas.cvetinskis@gmail.com>
2023-01-10 14:05:06 +0200

    apiroute dev variable changed to  production

--- present_server/Program.cs
+++ present_server/Program.cs
@@ -15,7 +15,7 @@ builder.Services.AddCors(options =>
     {
         builder.AllowAnyMethod()
         .AllowAnyHeader()
-        .WithOrigins("http://localhost:3000", "https://appname.azurestaticapps.net");
+        .WithOrigins("http://localhost:3000", "https://lively-bay-009ab1403.2.azurestaticapps.net");
         
     });
 });
